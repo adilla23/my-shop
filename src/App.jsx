fe1ebd44919b87cb635717650f50242a189f573b
adilla23 <48287931+adilla23@users.noreply.github.com>
2022-08-13 19:27:07 -0400

    test

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="flex max-w-lg mx-auto">
       <h1 className="border-2 rounded-lg mt-6 border-blue-100 p-6 shadow-lg bg-sky-200 text-white">
-        Hello world
+        Hello world2
       </h1>
     </div>
   );
