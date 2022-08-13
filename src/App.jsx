7e58b9da419f973a97526e4736158da894c29afb
adilla23 <48287931+adilla23@users.noreply.github.com>
2022-08-13 19:24:47 -0400

    Instalacion de tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="flex max-w-lg mx-auto">
+      <h1 className="border-2 rounded-lg mt-6 border-blue-100 p-6 shadow-lg bg-sky-200 text-white">
+        Hello world
+      </h1>
+    </div>
+  );
+}
+
+export default App;
